d731429d0188171c4f11c120d58ccaf76a900fc3
ekaiqbalv-dev <ekaiqbalvirgiawan.dev@gmail.com>
2023-02-07 19:54:04 +0700

    add example usage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
 import { Button, Space, DatePicker, Card } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { LineChart, Line } from "recharts";
 import Pagecontainer from "@/components/PageContainer";
 
 export default function Home() {
-  const data = [{ name: "Page A", uv: 100, pv: 2400, amt: 2400 },{ name: "Page B", uv: 200, pv: 2400, amt: 2400 }];
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+  const [chartData, setChartData] = useState([
+    { name: "Page A", uv: 100, pv: 400, amt: 1400 },
+    { name: "Page B", uv: 200, pv: 1400, amt: 2400 },
+  ]);
   const onChange = () => {};
+
+  async function getPageData() {
+    const apiUrlEndpoint = "/api/lala";
+    const response = await fetch(apiUrlEndpoint);
+    const res = await response.json();
+    console.log(res);
+  }
+
+  useEffect(() => {
+    console.log('cek')
+    getPageData();
+    setShouldAnimate(true);
+    setChartData((prev) => [
+      ...prev,
+      { name: "Page C", uv: 400, pv: 2400, amt: 3400 },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    console.log(chartData);
+  }, [chartData]);
   return (
     <Pagecontainer title="Coffee Shop">
       <div>Example</div>
@@ -18,8 +44,13 @@ export default function Home() {
           <p>Card content</p>
         </Card>
       </Space>
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <LineChart id={1} width={400} height={400} data={chartData}>
+        <Line
+          type="monotone"
+          dataKey="uv"
+          stroke="#8884d8"
+          isAnimationActive={shouldAnimate}
+        />
       </LineChart>
     </Pagecontainer>
   );
